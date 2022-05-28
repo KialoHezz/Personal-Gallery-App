03b6d859b881873d67e9adf6418893de3aeba73b
hezron <hezzykialo.official.ke@gmail.com>
2022-05-28 09:48:20 +0300

    create view for home,about,travel,search and food,also link css and templates inheritance

--- persgallary/gallary/urls.py
+++ persgallary/gallary/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from gallary.views import home_view
+from gallary.views import (home_view,about_view,index_view,travel_view,food_view)
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', home_view ,name='home-view'),
+    path('foods/',food_view,name='food_view'),
+    path('travelled-places/',travel_view,name='travel_view'),
+    path('index/' ,index_view ,name='index_view'),
+    path('about/', about_view , name='about_view'),
 ]
--- persgallary/gallary/views.py
+++ persgallary/gallary/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+    return render(request, 'index.html')
 
 def home_view(request):
 
     return render(request, 'home.html')
+
+
+def about_view(request):
+
+    return render(request, 'about.html')
+
+
+def travel_view(request):
+
+    return render(request, 'travel.html')
+
+def food_view(request):
+
+    return render(request, 'food.html')
