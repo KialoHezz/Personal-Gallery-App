26e1a9be11afbc6294fa0cec5051bbe551081375
hezron <hezzykialo.official.ke@gmail.com>
2022-05-28 10:51:52 +0300

    register or add the models to ADMIN Page

--- persgallary/gallary/admin.py
+++ persgallary/gallary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from gallary.models import Image,Location,Category
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
