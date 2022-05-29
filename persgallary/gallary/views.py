4697b8c744ab7635bfa6b4ba07f6f9cdead67074
hezron <hezzykialo.official.ke@gmail.com>
2022-05-29 18:50:11 +0300

    Query the Database:if ADMIN Add category now i can view it on front end

--- persgallary/gallary/views.py
+++ persgallary/gallary/views.py
@@ -16,49 +16,19 @@ def about_view(request):
     return render(request, 'about.html')
 
 
-def travel_view(request):
-
-    return render(request, 'travel.html')
-
-def food_view(request):
-
-    return render(request, 'food.html')
 
 
-def add_travel(request):
-    form = ImageForm()
-
-    if request.method == 'POST':
-        print(request.POST)
-        form = ImageForm(request.POST)
 
 
-        if form.is_valid():
-            form.save()
-
-        return render('travel')
-
-    else:
-
-        form = ImageForm(request.POST)
-
-    context = {
-        'form': form
-    }
-
-    return render(request, 'add_travel.html', context=context)
-
-
-def category(request):
-
+def categories_view(request):
     categories = Category.objects.all()
 
     cxt = {
             'categories': categories,
-            'images': images,
     }
 
-    return render(request, 'travel.html',cxt)
+    return render(request, 'categories.html',cxt)
+
 
 
 def view_image(request, pk):
